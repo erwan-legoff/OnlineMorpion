6d589e64c51f8563c8d6a56a34f8bbf1fde96307
Adrien-Corn <adrien.corn@etu.umontpellier.fr>
2020-05-24 17:34:50 +0200

    Interface

--- src/InterfaceGraphique/Interface.java
+++ src/InterfaceGraphique/Interface.java
@@ -1,14 +1,12 @@
 package InterfaceGraphique;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.*;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 
-import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -42,41 +40,46 @@ public class Interface extends JFrame {
         Panneau panneau = new Panneau();
         this.setContentPane(panneau);
 
+
+
         final int[] id = new int[1];
         int largeur = this.getWidth();
         int hauteur = this.getHeight();
 
         panneau.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e){
-                System.out.println("X = "+ e.getX()+" ; Y = "+e.getY());
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("X = " + e.getX() + " ; Y = " + e.getY());//temporaire
 
-                if(e.getY()<hauteur/3){
-                    if(e.getX()<largeur/3)
-                        id[0] =7;
-                    if(e.getX()>largeur/3&&e.getX()<largeur*2/3)
-                        id[0] =8;
-                    if(e.getX()>largeur*2/3)
-                        id[0] =9;
+                if (e.getY() < hauteur / 3) {
+                    if (e.getX() < largeur / 3)
+                        id[0] = 7;
+                    if (e.getX() > largeur / 3 && e.getX() < largeur * 2 / 3)
+                        id[0] = 8;
+                    if (e.getX() > largeur * 2 / 3)
+                        id[0] = 9;
                 }
-                if(e.getY()>hauteur/3&&e.getX()<hauteur*2/3){
-                    if(e.getX()<largeur/3)
-                        id[0] =4;
-                    if(e.getX()>largeur/3&&e.getX()<largeur*2/3)
-                        id[0] =5;
-                    if(e.getX()>largeur*2/3)
-                        id[0] =6;
+                if (e.getY() > hauteur / 3 && e.getX() < hauteur * 2 / 3) {
+                    if (e.getX() < largeur / 3)
+                        id[0] = 4;
+                    if (e.getX() > largeur / 3 && e.getX() < largeur * 2 / 3)
+                        id[0] = 5;
+                    if (e.getX() > largeur * 2 / 3)
+                        id[0] = 6;
                 }
-                if(e.getY()>hauteur*2/3){
-                    if(e.getX()<largeur/3)
-                        id[0] =1;
-                    if(e.getX()>largeur/3&&e.getX()<largeur*2/3)
-                        id[0] =2;
-                    if(e.getX()>largeur*2/3)
-                        id[0] =3;
+                if (e.getY() > hauteur * 2 / 3) {
+                    if (e.getX() < largeur / 3)
+                        id[0] = 1;
+                    if (e.getX() > largeur / 3 && e.getX() < largeur * 2 / 3)
+                        id[0] = 2;
+                    if (e.getX() > largeur * 2 / 3)
+                        id[0] = 3;
                 }
                 System.out.println(id[0]);
+
+                panneau.afficherPion(id[0]);
             }
 
         });
     }
+
 }
--- src/InterfaceGraphique/Panneau.java
+++ src/InterfaceGraphique/Panneau.java
@@ -5,8 +5,15 @@ import java.awt.*;
 
 public class Panneau extends JPanel {
 
-    Panneau(){
+    private int idJ;
+    private int posX;
+    private int posY;
+    private boolean nouvPion;
+    private boolean nouvPionAdversaire;
 
+    Panneau(){
+        nouvPion=false;
+        nouvPionAdversaire=false;
     }
 
     public void paintComponent(Graphics g){
@@ -18,7 +25,99 @@ public class Panneau extends JPanel {
                 g.drawString(String.valueOf(id), 10 + i * this.getWidth() / 3, 20 + j * this.getHeight() / 3);
             }
         }
+        if(nouvPion){
+            nouvPion=false;
+            g.drawString("X",posX,posY);
+        }
+        if(nouvPionAdversaire){
+            nouvPionAdversaire=false;
+            g.drawString("O",posX,posY);
+        }
+
     }
 
+    public void afficherPion(int id){
+        this.idJ=id;
+        if(id==7) {
+            this.posX = 100;
+            this.posY = 100;
+        }
+        if(id==8) {
+            this.posX = 300;
+            this.posY = 100;
+        }
+        if(id==9) {
+            this.posX = 500;
+            this.posY = 100;
+        }
+        if(id==4) {
+            this.posX = 100;
+            this.posY = 300;
+        }
+        if(id==5) {
+            this.posX = 300;
+            this.posY = 300;
+        }
+        if(id==6) {
+            this.posX = 500;
+            this.posY = 300;
+        }
+        if(id==1) {
+            this.posX = 100;
+            this.posY = 500;
+        }
+        if(id==2) {
+            this.posX = 300;
+            this.posY = 500;
+        }
+        if(id==3) {
+            this.posX = 500;
+            this.posY = 500;
+        }
+        this.nouvPion=true;
+        this.repaint();
+    }
+
+    public void afficherPionAdversaire(int id){
+        this.idJ=id;
+        if(id==7) {
+            this.posX = 100;
+            this.posY = 100;
+        }
+        if(id==8) {
+            this.posX = 300;
+            this.posY = 100;
+        }
+        if(id==9) {
+            this.posX = 500;
+            this.posY = 100;
+        }
+        if(id==4) {
+            this.posX = 100;
+            this.posY = 300;
+        }
+        if(id==5) {
+            this.posX = 300;
+            this.posY = 300;
+        }
+        if(id==6) {
+            this.posX = 500;
+            this.posY = 300;
+        }
+        if(id==1) {
+            this.posX = 100;
+            this.posY = 500;
+        }
+        if(id==2) {
+            this.posX = 300;
+            this.posY = 500;
+        }
+        if(id==3) {
+            this.posX = 500;
+            this.posY = 500;
+        }
+        this.nouvPionAdversaire=true;
+        this.repaint();
+    }
 
 }
